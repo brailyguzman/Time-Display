243456fcbbdd6335477a097325563bea73c1429d
Braily <94770717+brailyguzman@users.noreply.github.com>
2023-07-21 10:33:43 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    setInterval(displayTime, 1000)
+})
+
+function displayTime() {
+    const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const time = new Date();
+    const hours = time.getHours().toString().padStart(2, '0');
+    const minutes = time.getMinutes().toString().padStart(2, '0');
+    const seconds = time.getSeconds().toString().padStart(2, '0');
+
+    document.getElementById('hour').innerText = hours;
+    document.getElementById('minute').innerText = minutes;
+    document.getElementById('second').innerText = seconds;
+    document.getElementById('timezone').innerText = userTimeZone
+}
